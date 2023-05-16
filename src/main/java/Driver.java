bd69529423fac2c7ad37cd3d856de9276167d50b
Xiraj <akbarsirajuddinh@gmail.com>
2023-05-16 18:40:00 +0700

    default

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,7 +6,7 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Count Haha:" + counter.getCount());
+		System.out.println("Count" + counter.getCount());
 		
 		counter.increment();
 		
